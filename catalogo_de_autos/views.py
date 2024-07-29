08566b0bb87875c50bce3868ada698f6d354dc98
lfcepe <felipecepeda2006@hotmail.com>
2024-07-29 13:04:59 -0500

    Cambions en categorias y views

--- catalogo_de_autos/views.py
+++ catalogo_de_autos/views.py
@@ -12,34 +12,56 @@ def index_categoria(request):
     template = loader.get_template('index_categorias.html')
     return HttpResponse(template.render(request))
 
-def display_categoria(request):
-    template = loader.get_template('display_categoria.html')
-    return HttpResponse(template.render(request))
 
 def display_marca(request):
+    marcas = Marca.objects.order_by('marca')
     template = loader.get_template('display_marca.html')
-    return HttpResponse(template.render(request))
+    return HttpResponse(template.render({'marcas': marcas})(request))
+
+def obtener_marca(marca_id):
+    marca = Marca.objects.get(pk = marca_id)
+    return {'marca': marca}    
 
 def display_colorauto(request):
+    colores = ColorAuto.objects.order_by('colorauto')
     template = loader.get_template('display_colorauto.html')
-    return HttpResponse(template.render(request))
+    return HttpResponse(template.render({'colores' : colores})(request))
+
+def obtener_colores(color_id):
+    color = ColorAuto.objects.get(pk = color_id)
+    return {'color': color}
     
 def display_tipocarro(request):
+    tipos = TipoCarro.objects.order_by('modelocarro')
     template = loader.get_template('display_tipocarro.html')
-    return HttpResponse(template.render(request))
+    return HttpResponse(template.render({'tipos': tipos})(request))
+
+def obtener_tipocarro(tipo_id):
+    tipo = TipoCarro.objects.get(pk= tipo_id)
+    return {'tipo': tipo}
+
 
 def display_iva(request):
+    piva = Iva.objects.order_by('valordecalculo')
     template = loader.get_template('display_iva.html')
-    return HttpResponse(template.render(request))
+    return HttpResponse(template.render({'piva': piva})(request))
+
+def obtener_iva(iva_id):
+    iva = Iva.objects.get(pk = iva_id)
+    return {'iva': iva}
     
 def display_formapago(request):
+    pagos = FormaPago.objects.order_by('formadepago')
     template = loader.get_template('display_formapago.html')
-    return HttpResponse(template.render(request))
+    return HttpResponse(template.render({'pagos': pagos})(request))
 
+def obtener_formadepago(formadepago_id):
+    pago = FormaPago.objects.get(pk = formadepago_id)
+    return {'pago': pago}
 
 def display_cliente(request):
     clientes = Cliente.objects.order_by('apellido')
-    templade = loader.get_templade('display_clientes.html')
+    templade = loader.get_template('display_clientes.html')
     return HttpResponse(templade.render({'clientes': clientes}), request)
 
 def index_autos(request):
@@ -61,6 +83,9 @@ def display_kardex(request):
     template = loader.get_template('display_kardex.html')
     return HttpResponse(template.render({'kardexs': kardexs}, request))
 
+def obtener_kardex(kardex_id):
+    kardex = Kardex.objects.get(pk = kardex_id)
+    return {'kardex': kardex}
 
 def display_ventas(request):
     ventas = Venta.objects.order_by('fechacompra')
