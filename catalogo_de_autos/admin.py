3b1494b1dc81e4edcc89a736add4a64e74e51b97
lfcepe <felipecepeda2006@hotmail.com>
2024-07-30 11:20:58 -0500

    Cambios en admin

--- catalogo_de_autos/admin.py
+++ catalogo_de_autos/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
-
+from .models import Marca, TipoCarro, ColorAuto,Iva, FormaPago, Cliente, Auto, Kardex, Venta
 # Register your models here.
+
+@admin.register(Marca)
+class MarcaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TipoCarro)
+class TipoCarroAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ColorAuto)
+class ColorAutoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Iva)
+class IvaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(FormaPago)
+class FormaPagoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Kardex)
+class KardexAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    pass
