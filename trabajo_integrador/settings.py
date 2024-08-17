b978945f49410941c994ee6e979a3a92759ca2c4
Josue <jatipanb@puce.edu.ec>
2024-08-16 21:19:39 -0500

    Proyecto Terminado

--- trabajo_integrador/settings.py
+++ trabajo_integrador/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'NAME': 'catalogo_autos',
         'HOST': 'localhost',
         'PORT': '5432',
-        'USER': 'felipecepeda',
-        'PASSWORD': 'Ale+dra123'
+        'USER': 'josuetb',
+        'PASSWORD': '1753'
    }
 }
 
